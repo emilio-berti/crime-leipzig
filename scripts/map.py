aaafc9857062be6e23ef01821c4e9c2788b8d7dd
Emilio <emilio.berti90@gmail.com>
2024-07-09 14:52:53 +0200

    :hammer: :construction: some locations still inaccurate

--- scripts/map.py
+++ scripts/map.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
@@ -5,46 +6,55 @@ from shapely.geometry import Polygon
 from geopy.geocoders import Nominatim
 import plotly.express as px
 
-# read crime csv --------
-d = pd.read_csv("data/crimes.csv")
-places = d.place
-streets = d.street
+if len(sys.argv) == 1:
+    clean = True
+else:
+    clean = sys.argv[1]
 
-# locate addresses ----------
-geolocator = Nominatim(user_agent = "GetLoc")
-lat = [None for x in places]
-lon = [None for x in places]
-for i in range(len(places)):
-    print("  -", i + 1, "of", len(places))
-    if (streets[i] != streets[i]): #street is nan
-        coords = geolocator.geocode(places[i]) #use only place
-    else:
-        coords = geolocator.geocode(streets[i] + ', ' + places[i]) #use also street
-    if coords is None:
-        lat[i] = None
-        lon[i] = None
-    else:
-        lat[i] = coords[1][0]
-        lon[i] = coords[1][1]
+if clean:
+    # read crime csv --------
+    d = pd.read_csv("data/crimes.csv")
+    places = d.place
+    streets = d.street
 
-# remove locations that were not found
-while None in lat:
-    empty = lat.index(None)
-    lat.pop(empty)
-    lon.pop(empty)
-    d = d.drop(empty)
+    # locate addresses ----------
+    geolocator = Nominatim(user_agent = "GetLoc")
+    lat = [None for x in places]
+    lon = [None for x in places]
+    for i in range(len(places)):
+        print("  -", i + 1, "of", len(places))
+        if (streets[i] != streets[i]): #street is nan
+            coords = geolocator.geocode(places[i]) #use only place
+        else:
+            coords = geolocator.geocode(streets[i] + ', ' + places[i]) #use also street
+        if coords is None:
+            lat[i] = None
+            lon[i] = None
+        else:
+            lat[i] = coords[1][0]
+            lon[i] = coords[1][1]
 
-geom = [Point(lon[i], lat[i]) for i in range(len(lat))]
-d['geometry'] = geom
-d = gpd.GeoDataFrame(d, crs = "EPSG:4326")
-s = [1 if x != x else 3 for x in d.street]
-d['size'] = s
+    # remove locations that were not found ----------
+    while None in lat:
+        empty = lat.index(None)
+        lat.pop(empty)
+        lon.pop(empty)
+        d = d.drop(empty)
 
+    # create geodataframe --------
+    geom = [Point(lon[i], lat[i]) for i in range(len(lat))]
+    d['geometry'] = geom
+    d = gpd.GeoDataFrame(d, crs = "EPSG:4326")
+    s = [1 if x != x else 3 for x in d.street]
+    d['size'] = s
+    d.to_file("data/crimes.shp")
+
+geo = gpd.read_file("data/crimes.shp")
 px.set_mapbox_access_token(open(".mapbox_token").read())
 fig = px.scatter_mapbox(
-    d,
-    lat = d.geometry.y,
-    lon = d.geometry.x,
+    geo,
+    lat = geo.geometry.y,
+    lon = geo.geometry.x,
     size = "size",
     opacity = 0.75,
     hover_name = "title",
