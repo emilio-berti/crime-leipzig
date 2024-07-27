b0f07385f4fb03b11658914455bd48f062600fd6
Emilio <emilio.berti90@gmail.com>
2024-07-27 08:50:16 +0200

    :hammer: geocrime submoduled

---
+++ geocrime/map.py
@@ -0,0 +1,66 @@
+import plotly.express as px
+import plotly.io as pio
+from geopy.geocoders import Nominatim
+import geopandas as gpd
+from shapely.geometry import Point
+
+def geolocate(d):
+    place = d.place
+    street = d.street
+    geolocator = Nominatim(user_agent = "GetLoc")
+    lat = [None for x in place]
+    lon = [None for x in place]
+    print(" - Geolocating...")
+    for i in range(len(place)):
+        print("   -", i + 1, "of", len(place))
+        if (street[i] != street[i]): #street is nan
+            coords = geolocator.geocode(place[i]) #use only place
+        else:
+            coords = geolocator.geocode(street[i] + ', ' + place[i]) #use also street
+        if coords is None:
+            lat[i] = None
+            lon[i] = None
+        else:
+            lat[i] = coords[1][0]
+            lon[i] = coords[1][1]
+
+    # remove rows without location -----------
+    print(" - Cannot find:")
+    while None in lat:
+        empty = lat.index(None)
+        print("   - " + d['place'][empty])
+        lat.pop(empty)
+        lon.pop(empty)
+        d = d.drop(empty)
+
+    # add geo columns -----------
+    d['lat'] = lat
+    d['lon'] = lon
+    return d
+
+def shape(d, write = True):
+    # create geodataframe --------
+    geom = [Point(d.lon[i], d.lat[i]) for i in range(len(d))]
+    d['geometry'] = geom
+    d = gpd.GeoDataFrame(d, crs = "EPSG:4326")
+    s = [1 if x != x else 3 for x in d.street]
+    d['size'] = s
+    if write: d.to_file("data/crimes.shp")
+    return d
+
+def map(geo):
+    px.set_mapbox_access_token(open(".mapbox_token").read())
+    fig = px.scatter_mapbox(
+        geo,
+        lat = geo.geometry.y,
+        lon = geo.geometry.x,
+        size = "size",
+        opacity = 0.75,
+        hover_name = "title",
+        hover_data = ["title", "place", "street", "date", "people"],
+        zoom = 10,
+        color = "title",
+        mapbox_style = "satellite-streets"
+    )
+    pio.write_image(fig, "figures/map", format = "pdf")
+    fig.show()
--- scripts/map.py
+++ scripts/map.py
@@ -1,71 +1,10 @@
-import sys
-import pandas as pd
+import pandas as pd 
 import geopandas as gpd
-from shapely.geometry import Point
-from shapely.geometry import Polygon
-from geopy.geocoders import Nominatim
-import plotly.express as px
-import plotly.io as pio
+from geocrime import map
 
-if len(sys.argv) == 1:
-    clean = True
-else:
-    clean = sys.argv[1]
-    clean = True if clean == "True" else False
-
-if clean:
-    # read crime csv --------
-    d = pd.read_csv("data/crimes.csv")
-    places = d.place
-    streets = d.street
-
-    # locate addresses ----------
-    geolocator = Nominatim(user_agent = "GetLoc")
-    lat = [None for x in places]
-    lon = [None for x in places]
-    for i in range(len(places)):
-        print("  -", i + 1, "of", len(places))
-        if (streets[i] != streets[i]): #street is nan
-            coords = geolocator.geocode(places[i]) #use only place
-        else:
-            coords = geolocator.geocode(streets[i] + ', ' + places[i]) #use also street
-        if coords is None:
-            lat[i] = None
-            lon[i] = None
-        else:
-            lat[i] = coords[1][0]
-            lon[i] = coords[1][1]
-
-    # remove locations that were not found ----------
-    print(" - Cannot find:")
-    while None in lat:
-        empty = lat.index(None)
-        print("   - " + d['place'][empty])
-        lat.pop(empty)
-        lon.pop(empty)
-        d = d.drop(empty)
-
-    # create geodataframe --------
-    geom = [Point(lon[i], lat[i]) for i in range(len(lat))]
-    d['geometry'] = geom
-    d = gpd.GeoDataFrame(d, crs = "EPSG:4326")
-    s = [1 if x != x else 3 for x in d.street]
-    d['size'] = s
-    d.to_file("data/crimes.shp")
+d = pd.read_csv("data/crimes.csv")
+d = map.geolocate(d)
+# geo = shape(geo)
 
 geo = gpd.read_file("data/crimes.shp")
-px.set_mapbox_access_token(open(".mapbox_token").read())
-fig = px.scatter_mapbox(
-    geo,
-    lat = geo.geometry.y,
-    lon = geo.geometry.x,
-    size = "size",
-    opacity = 0.75,
-    hover_name = "title",
-    hover_data = ["title", "place", "street", "date", "people"],
-    zoom = 10,
-    color = "title",
-    mapbox_style = "satellite-streets"
-)
-pio.write_image(fig, "figures/map", format = "pdf")
-fig.show()
+map.map(geo)
